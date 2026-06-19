386364808344b8fdb56b77411a9d54950163a21d
user.name : "Ayush <kumarayush8117@gmail.com>
2026-06-19 15:10:31 +0530

    so , Here we done my o2-Project with learning nad sone my all Component Basic Understanding series by shreyansh coading school.

---
+++ 02-Project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './component/Card'
+import Navbar from './component/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Card />
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ 02-Project/src/component/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+
+      <div className="card">
+        <h1>Sharthik Sharma</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, illo
+          aliquam explicabo sint ea soluta consectetur alias cupiditate
+          deleniti. Quasi!
+        </p>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Card;
