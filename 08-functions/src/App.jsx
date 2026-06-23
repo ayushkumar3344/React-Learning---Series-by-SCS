88e290254c27bfeb7ae825242b011dd5bd7ffa85
user.name : "Ayush <kumarayush8117@gmail.com>
2026-06-23 22:39:12 +0530

    fIn this understand all the function basics.

---
+++ 08-functions/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+
+  function btnClicked(){
+    console.log('Hello');
+  }
+
+  return (
+    <div>
+      <button>Click</button>
+    </div>
+  )
+
+}
+
+export default App
